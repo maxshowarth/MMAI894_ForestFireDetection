8096080b0bccb78d19dcb0259cd4e0222b60830c
max <maxshowarth@gmail.com>
2020-02-21 13:45:37 -0500

    Commit stuff from old github

--- scratch_Image_Sorter.py
+++ scratch_Image_Sorter.py
@@ -6,7 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import cv2
-
+print(os.getcwd())
+os.chdir(os.path.dirname("/Users/max/Quick Jupyter Notebooks/MMAI/MMAI 894 - Deep Learning/"))
+print(os.getcwd())
 smokeDir = os.path.dirname("/sorted_images/smoke/")
 unsureDir = os.path.dirname("/sorted_images/unsure/")
 clearDir = os.path.dirname("/sorted_images/clear/")
@@ -29,13 +31,15 @@ for sourceImage in tileInfo_df['sourceFile'].unique():
     cv2.destroyAllWindows()
     print(sourceImage)
     sourceImageTiles = tileInfo_df[tileInfo_df['sourceFile'] == sourceImage]
+    cv2.namedWindow("Source_Image")
+    cv2.namedWindow("Tile")
     for index, tile in sourceImageTiles.iterrows():
         cv2.destroyAllWindows()
         movePath = ""
         tilePath = os.path.normpath(tile['tilePath'])
         tile_img = cv2.imread(tilePath)
-        cv2.imshow("tile", tile_img)
-        cv2.waitKey(0)
+        cv2.imshow("Tile", tile_img)
+        cv2.waitKey(1)
         while movePath == "":
             print(tile['tileName'])
             kb_input = input("Enter choice: ")
@@ -52,8 +56,8 @@ for sourceImage in tileInfo_df['sourceFile'].unique():
                 rectImage = cv2.rectangle(sourceImageLoaded, (int(splitName[1]), int(splitName[2])), (tileSize, tileSize), (255, 0, 0), 15)
                 height, width, channels = rectImage.shape
                 rectImageSmall = cv2.resize(rectImage, (height // 10, width // 10))
-                cv2.imshow('image', rectImageSmall)
-                cv2.waitKey(0)
+                cv2.imshow('Source_Image', rectImageSmall)
+                cv2.waitKey(1)
                 cv2.destroyAllWindows()
                 print("no move")
             if kb_input == 's':
@@ -61,4 +65,3 @@ for sourceImage in tileInfo_df['sourceFile'].unique():
                 movePath = "skipped"
                 print(movePath)
                 print("test")
-        cv2.destroyAllWindows()
