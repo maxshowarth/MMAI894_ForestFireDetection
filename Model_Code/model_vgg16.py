99d62346f91ef2906c7effce651155860d3efb59
max <maxshowarth@gmail.com>
2020-03-20 15:24:45 -0400

    fixing uncommented comment

--- Model_Code/model_vgg16.py
+++ Model_Code/model_vgg16.py
@@ -72,7 +72,7 @@ def showNumpyImage(image):
 if os.path.isdir('./model_cache/VGG16_cache'):
     print("Model Cache Exists")
 else:
-    os.mkdirs("./model_cache/VGG16_cache")
+    os.makedirs("./model_cache/VGG16_cache")
     print("Created Model Cache")
 
 bucket_files = [blob.name for blob in storage_client.list_blobs(bucket_name)]
